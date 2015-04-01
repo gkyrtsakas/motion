dc0340df67089447ef046eaf2029793d20d984fa
gsakis <gkyrtsakas@gmail.com>
2015-04-01 11:57:54 -0400

    Optical flow working
    Now finds good features and tracks them through the video

--- opticFlow.py
+++ opticFlow.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture('backSubOut.mp4')
 
 # params for ShiTomasi corner detection
 feature_params = dict( maxCorners = 100,
-                       qualityLevel = 0.1,
+                       qualityLevel = 0.9,
                        minDistance = 7,
                        blockSize = 7 )
 
@@ -18,13 +18,18 @@ lk_params = dict( winSize  = (15,15),
 # Create some random colors
 color = np.random.randint(0,255,(100,3))
 
+def findGoodFeatures (old_frame):
+    # ret, old_frame = cap.read()
+    old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+    p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+    return p0, old_gray
+
 # Take first frame and find corners in it
 p0 = None
 
 while p0 == None:
     ret, old_frame = cap.read()
-    old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-    p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+    p0, old_gray = findGoodFeatures (old_frame)
 
 
 # Create a mask image for drawing purposes
@@ -36,11 +41,16 @@ while(1):
         break;
 
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
+
+
 
     # calculate optical flow
-    
+    if (p0.size < 10):
+        p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
     p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+
+    # print "p0 = " + str(p0.size)
+    # print "p1 = " + str(p1.size)
 
     # Select good points
    
@@ -51,9 +61,9 @@ while(1):
     for i,(new,old) in enumerate(zip(good_new,good_old)):
         a,b = new.ravel()
         c,d = old.ravel()
-        if (abs(a-c) > 0.1 and abs(b-d) > 0.1):
-            cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-            cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
+        # if (abs(a-c) > 0.1 and abs(b-d) > 0.1):
+        cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
+        cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
     #print type(frame)
     #print type(mask)
     img = cv2.add(frame,mask)
