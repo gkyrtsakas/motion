a01b5c1dfaf6bed3f976bf03b0c1728d87428a79
gsakis <gkyrtsakas@gmail.com>
2015-04-04 14:37:27 -0400

    Detects objects entering from left

--- hybrid.py
+++ hybrid.py
@@ -1,13 +1,15 @@
 import numpy as np
 import cv2
+import pdb
+import time
 
-cap = cv2.VideoCapture('footage3.mp4')
+cap = cv2.VideoCapture('Highway2.m4v')
 #cap = cv2.VideoCapture('footage3.mp4')
 
 # params for ShiTomasi corner detection
 feature_params = dict( maxCorners = 100,
-                       qualityLevel = 0.9,
-                       minDistance = 7,
+                       qualityLevel = 0.01,
+                       minDistance = 15,
                        blockSize = 7 )
 
 # Parameters for lucas kanade optical flow
@@ -31,59 +33,95 @@ def findGoodFeatures (old_gray):
 # Take first frame and find corners in it
 p0 = None
 
+# pdb.set_trace()
+
 while p0 == None:
     ret, old_frame = cap.read()
     mog_image  = mog.apply(old_frame)
     p0, old_gray = findGoodFeatures (mog_image)
+    # oldHist = cv2.calcHist([old_gray], [0], None,[256], [0,256])
+    # print p0
+    # print type(p0)
+
 
 
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
-
+# newHist = oldHist
 
 
 while(1):
+    
+    # pdb.set_trace();
+    Flag = False;
+    # main_time = time.time()
     ret,frame = cap.read()
+    # print time.time() - main_time
     if ret == False:
         break;
-
+    
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     frame_gray  = mog.apply(frame)
+    
+    enter_left = frame_gray[:,0]
+
+
+    # print enter_left > 0;
+
+    # for i in enter_left:
+    #     if enter_left[i] > 0:
+    #         flag = 1;
+    #         break;
+    # pdb.set_trace()
+    Flag = any(enter_left > 0)
+    # oldHist = newHist
+    # newHist = cv2.calcHist([frame_gray], [0], None,[256], [0,256])
+    # diff = cv2.compareHist(oldHist, newHist, 3)
 
     # calculate optical flow
-    if (p0.size < 10):
+    if (p0.size < 10 or Flag):
+        
         p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+        # p0 = np.concatenate((p0,temp))
         mask = np.zeros_like(old_frame)
+
     p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
     # print "p0 = " + str(p0.size)
     # print "p1 = " + str(p1.size)
 
     # Select good points
-   
+    # pdb.set_trace();
     good_new = p1[st==1]
     good_old = p0[st==1]
 
     # draw the tracks
+    
     for i,(new,old) in enumerate(zip(good_new,good_old)):
         a,b = new.ravel()
         c,d = old.ravel()
         # if (abs(a-c) > 0.1 and abs(b-d) > 0.1):
+
         cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
         cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
+    
     #print type(frame)
     #print type(mask)
     img = cv2.add(frame,mask)
 
     cv2.imshow('frame',img)
-    k = cv2.waitKey(30) & 0xff
+    # print frame_gray.shape
+    
+    k = cv2.waitKey(1)
     if k == 27:
         break
-
+   
     # Now update the previous frame and previous points
-    old_gray = frame_gray.copy()
+    old_gray = frame_gray
     p0 = good_new.reshape(-1,1,2)
+    
+    
    
 
 cv2.destroyAllWindows()
